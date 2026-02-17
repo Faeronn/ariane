78af7932b781f4a827b50b01a9e779c272e458d7
Faeronn <alexis.lelardoux@hotmail.fr>
2026-02-17 14:52:14 +0100

    added refresh_token to the schema

--- ariane.sql
+++ ariane.sql
@@ -11,7 +11,7 @@
  Target Server Version : 110400
  File Encoding         : 65001
 
- Date: 16/02/2026 13:53:56
+ Date: 17/02/2026 14:51:34
 */
 
 SET NAMES utf8mb4;
@@ -28,6 +28,24 @@ CREATE TABLE `domain`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
+-- Table structure for refresh_token
+-- ----------------------------
+DROP TABLE IF EXISTS `refresh_token`;
+CREATE TABLE `refresh_token`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL,
+  `tokenHash` char(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `expiresAt` datetime NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `lastUsedAt` datetime NULL DEFAULT NULL,
+  `revokedAt` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uniq_tokenHash`(`tokenHash`) USING BTREE,
+  INDEX `idx_userID`(`userID`) USING BTREE,
+  CONSTRAINT `refresh_token_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_520_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
 -- Table structure for users
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
@@ -38,9 +56,9 @@ CREATE TABLE `users`  (
   `firstname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NULL DEFAULT NULL,
   `lastname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NULL DEFAULT NULL,
   `role` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NULL DEFAULT NULL,
-  `isVerified` tinyint(1) NULL DEFAULT NULL,
+  `isVerified` tinyint NULL DEFAULT NULL,
   PRIMARY KEY (`userID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_520_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_520_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for verifications
@@ -51,6 +69,6 @@ CREATE TABLE `verifications`  (
   `userID` int NULL DEFAULT NULL,
   `verificationCode` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NULL DEFAULT NULL,
   PRIMARY KEY (`verificationID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_520_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_520_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
